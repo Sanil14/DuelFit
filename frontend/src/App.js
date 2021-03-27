32198aed87817a0eba08baea117b3d0b6dd89ff2
Niranjan420 <niranjanrajesh02@gmail.com>
2021-03-27 22:12:59 +0530

    Initialized Tailwind + Landing Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Duel Fit</h1>
+        <LandingPage />
       </header>
     </div>
   );
