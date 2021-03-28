f8abce9a744e652942ebe39d69b1e69d4cc83354
Sanil14 <saniljalan14@gmail.com>
2021-03-28 17:19:58 +0530

    add queue API

--- backend/nodejs/src/controllers.js
+++ backend/nodejs/src/controllers.js
@@ -32,15 +32,54 @@ const controller = {
     },
 
     joinQueue: async (req, res) => {
+        const checkQueue = await controller._checkInQueue(req.uid);
+        if (checkQueue) return res.errorMessage(`Already in queue`);
         newqueueuser = db().ref(`/queue`).push()
         newqueueuser.set({
             uid: req.uid
         });
+        const checkFull = await controller._checkQueueFull();
+        if (checkFull) return res.success({ gamestarted: true })
+        return res.success({ gamestarted: false });
     },
 
-    _checkInQueue: async () => {
+    _checkInQueue: async (user) => {
         const snapshot = await db().ref(`queue`).get();
-        console.log(snapshot.val());
+        if (!snapshot.val()) return false;
+        if (Object.values(snapshot.val()).filter(u => u.uid === user).length > 0) return true;
+        return false;
+    },
+
+    _checkQueueFull: async () => {
+        const snapshot = await db().ref('queue').get();
+        if (Object.values(snapshot.val()).length === 2) {
+            const uids = [Object.values(snapshot.val())[0].uid, Object.values(snapshot.val())[1].uid];
+            await controller._startMatch(uids);
+            await controller._resetQueue();
+            return true;
+        };
+        return false;
+    },
+
+    _startMatch: async (uids) => {
+        const datastructure = {
+            "jj": {
+                "5L7hL288O4YyDkT2m5A9Sk4Rq783": 10,
+                "6OYEc44IjIVuhIaUntOe9rguM653": 10
+            },
+            "squats": {
+                "5L7hL288O4YyDkT2m5A9Sk4Rq783": 10,
+                "6OYEc44IjIVuhIaUntOe9rguM653": 20
+            }
+        }
+    },
+
+    _resetQueue: async () => {
+        await db().ref('queue').remove();
+    },
+
+    _setupMatch: async (uids) => {
+
     }
 }
 
--- backend/nodejs/src/routes.js
+++ backend/nodejs/src/routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { registerUser, getProfile } = require("./controllers");
+const { registerUser, getProfile, joinQueue } = require("./controllers");
 const { populateReq, userAuth } = require("./middleware");
 
 router.use(populateReq);
@@ -7,5 +7,7 @@ router.use(populateReq);
 router.post('/user/register', registerUser);
 
 router.get('/user/profile', userAuth, getProfile);
+
+router.get('/game/joinqueue', userAuth, joinQueue);
 
 module.exports = router;
