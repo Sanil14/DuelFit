d5ea6cd65e899cb99e6021bbf6a2784eef02caa8
Aryan Deshpande <deshpandearyan1@gmail.com>
2021-03-28 06:28:38 +0530

    Screen 3 done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import FindGame from './components/FindGame/FindGame';
 import GameScreen1 from './components/GameScreen1/GameScreen1';
+import GameScreen3 from './components/GameScreen3/GameScreen3';
 import GameScreen4 from './components/GameScreen4/GameScreen4';
 import LandingPage from './components/LandingPage/LandingPage';
 import LaterRegister from './components/Later Register/LaterRegister';
@@ -19,6 +20,7 @@ function App() {
         {/* <MainMenu /> */}
         <FindGame />
         <GameScreen1 />
+        <GameScreen3 />
         <GameScreen4 />
       </header>
     </div>
